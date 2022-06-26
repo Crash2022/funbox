57d162f89f8e2efafcaddf1eecab101f84bf2e20
Crash2022 <97619755+Crash2022@users.noreply.github.com>
2022-06-26 13:27:26 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-/* const imageBody = document.querySelector('.find');
-document.addEventListener('click', input);
-
-function input(event) {
-  if (event.target.closest('.find__image')) {
-    imageBody.classList.toggle('_active');
-    console.log('123');
-  }
-  if (!event.target.closest('.find')) {
-    imageBody.classList.remove('_active');
-    console.log('456');
-  }
-}
-
-document.addEventListener("keyup", esc);
-function esc(event) {
-  if (event.code === 'Escape') {
-    console.log('Done!');
-    imageBody.classList.remove('_active');
-  }
-}
-
-const textInput = document.querySelector('.find__input');
-const textInputLimit = textInput.getAttribute('maxlength');
-const textInputCounter = document.querySelector('.find__counter span');
-textInputCounter.innerHTML = textInputLimit;
-
-textInput.addEventListener("keyup", textSetCounter);
-
-function textSetCounter() {
-  const textInputResult = textInputLimit - textInput.value.length;
-  textInputCounter.innerHTML = textInputResult;
-} */
-
-/* --------------------------------------------------------------------------- */
-
 "use strict"
 
 const buyButton = document.querySelectorAll('.buy');
